a9f733ff0d85964367a33e8d843a737e3e0b23fd
RenatoSandro <renatoalves311@gmail.com>
2022-06-02 14:30:43 -0300

    novos codigos

---
+++ parametros.js
@@ -0,0 +1,17 @@
+// parametros de função
+
+//function soma (num1, num2) {
+//return num1 + num2;
+//}
+
+//console.log(soma (26,5))
+//console.log(soma(2,5))
+//console.log(soma(36, 2))
+
+// parametros * argumentos
+
+function dadosCompletos ( nome, idade, endereço, cpf,) {
+    return `Meu nome é ${nome} , e tenho a idade de ${idade} , meu endereço é ${endereço} , e o numero do meu cpf é ${cpf}`;
+
+}
+console.log(dadosCompletos("Renato", 26 , "Rua Reginaldo de Souza Lima" , "129.555.555.00"))
